e2713788bcf6bda09d2635eb06cf755634f15e46
Emerson Bezerra <120873878+emersonbbezerra@users.noreply.github.com>
2025-08-07 01:58:21 +0000

    config hosts

--- vite.config.ts
+++ vite.config.ts
@@ -29,27 +29,31 @@ export default defineConfig({
     emptyOutDir: true,
   },
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 3000,
-    allowedHosts: 'all',
+    allowedHosts: true,
     fs: {
       strict: true,
       deny: ["**/.*"],
     },
     proxy: {
-      '/api': {
-        target: 'http://0.0.0.0:5000',
+      "/api": {
+        target: "http://0.0.0.0:5000",
         changeOrigin: true,
         secure: false,
         configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
+          proxy.on("error", (err, _req, _res) => {
+            console.log("proxy error", err);
           });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
+          proxy.on("proxyReq", (proxyReq, req, _res) => {
+            console.log("Sending Request to the Target:", req.method, req.url);
           });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          proxy.on("proxyRes", (proxyRes, req, _res) => {
+            console.log(
+              "Received Response from the Target:",
+              proxyRes.statusCode,
+              req.url,
+            );
           });
         },
       },
