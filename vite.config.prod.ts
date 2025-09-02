8e094718b7548c0137372b85276223dc97d4d5a3
Emerson Bezerra <emersonbbezerra@gmail.com>
2025-09-02 13:56:00 -0300

    feat: remove Tailwind CSS plugin from Vite configuration

--- vite.config.prod.ts
+++ vite.config.prod.ts
@@ -1,10 +1,12 @@
-import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  css: {
+    postcss: './postcss.config.js',
+  },
   resolve: {
     alias: {
       '@': path.resolve(import.meta.dirname, 'client', 'src'),
